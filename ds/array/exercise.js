f8d74ebb10a523d2e352bfdabec7438adfa73509
Muhamad Syihabudin <syehab94@gmail.com>
2026-02-08 20:00:53 +0700

    Instantiation

--- ds/array/exercise.js
+++ ds/array/exercise.js
@@ -22,3 +22,26 @@ const obj4 = {
     }
 }
 
+// instantiation
+// is a way to create multiple instances of an object type
+class Player {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    introduce() {
+        console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+    }
+}
+
+class Wizard extends Player {
+    constructor(name, type) {
+        super(name, type);
+    }
+    play() {
+        console.log(`WEEEE I'm a ${this.type}`);
+    }
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Shawn', 'Dark Magic');
