b957a1fc9ef57576d7dac46b4a1924109b20c7f0
Muhamad Syihabudin <syehab94@gmail.com>
2026-02-17 21:18:10 +0700

    Refactor code

--- ds/array/merge_sorted_arrays2.js
+++ ds/array/merge_sorted_arrays2.js
@@ -1,4 +1,4 @@
-function mergeSortedArrays(array1, array2){
+function mergeSortedArrays(array1, array2) {
     const mergedArray = [];
     let array1Item = array1[0];
     let array2Item = array2[0];
@@ -6,25 +6,25 @@ function mergeSortedArrays(array1, array2){
     let j = 1;
     
     //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
-    if(array1.length === 0) {
-      return array2;
+    if (array1.length === 0) {
+        return array2;
     }
-    if(array2.length === 0) {
-      return array1;
+    if (array2.length === 0) {
+        return array1;
     }
   
     // We need to keep track of the current index of each array and compare the current items of both arrays. We will push the smaller item into the merged array and move the index of that array forward. We will continue this process until we have gone through both arrays.
-    while (array1Item || array2Item){
-     if(array2Item === undefined || array1Item < array2Item){
-       mergedArray.push(array1Item);
-       array1Item = array1[i];
-       i++;
-     }   
-     else {
-       mergedArray.push(array2Item);
-       array2Item = array2[j];
-       j++;
-     }
+    while (array1Item || array2Item) {
+
+        if (array2Item === undefined || array1Item < array2Item){
+            mergedArray.push(array1Item);
+            array1Item = array1[i];
+            i++;
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = array2[j];
+            j++;
+        }
     }
     return mergedArray;
   }
